718af198b54c17ea8672d5c17e47eca8a6db70d3
lbalbrecht <lalbrecht18@wooster.edu>
2021-05-18 20:58:12 -0700

    got dates to work

--- script.js
+++ script.js
@@ -14,18 +14,7 @@ $(document).ready(function () {
         }).then(function (response) {
             console.log(response);
             var todayHeader = $('#name-date')
-            var today = new Date()
-            var dd = today.getDate();
-            var mm = today.getMonth();
-            var yyyy = today.getFullYear();
-            if (dd < 10) {
-                dd = '0' + dd;
-            };
-            if (mm < 10) {
-                mm = '0' + mm;
-            };
-            today = '(' + mm + '/' + dd + '/' + yyyy + ')'
-            todayHeader.append(cityInput + ' ' + today)
+            todayHeader.append(cityInput + ' ' + (moment().format('l')));
 
             $("#temp").append("Temperature: " + ((response.main.temp - 273.15) * 1.8 + 32).toFixed(1) + `\xB0` + 'F')
             $("#humid").append("Humidity: " + (response.main.humidity) + '%')
@@ -36,22 +25,27 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
             console.log(response);
+            $("#date1").append((moment().add(1, 'days').format('l')));
             $("#temp1").append('Temperature: ' + (((response.list[0].main.temp - 273.15) * 1.8 + 32).toFixed(1) + `\xB0` + 'F'));
             $('#humid1').append('Humidity: ' + ((response.list[0].main.humidity) + '%'));
             $('#wind1').append('Wind speed: ' + ((response.list[0].wind.speed) + 'mph'));
 
+            $("#date2").append((moment().add(2, 'days').format('l')));
             $("#temp2").append('Temperature: ' + (((response.list[8].main.temp - 273.15) * 1.8 + 32).toFixed(1) + `\xB0` + 'F'));
             $('#humid2').append('Humidity: ' + ((response.list[8].main.humidity) + '%'));
             $('#wind2').append('Wind speed: ' + ((response.list[8].wind.speed) + 'mph'));
 
+            $("#date3").append((moment().add(3, 'days').format('l')));
             $("#temp3").append('Temperature: ' + (((response.list[16].main.temp - 273.15) * 1.8 + 32).toFixed(1) + `\xB0` + 'F'));
             $('#humid3').append('Humidity: ' + ((response.list[16].main.humidity) + '%'));
             $('#wind3').append('Wind speed: ' + ((response.list[16].wind.speed) + 'mph'));
 
+            $("#date4").append((moment().add(4, 'days').format('l')));
             $("#temp4").append('Temperature: ' + (((response.list[24].main.temp - 273.15) * 1.8 + 32).toFixed(1) + `\xB0` + 'F'));
             $('#humid4').append('Humidity: ' + ((response.list[24].main.humidity) + '%'));
             $('#wind4').append('Wind speed: ' + ((response.list[24].wind.speed) + 'mph'));
 
+            $("#date5").append((moment().add(5, 'days').format('l')));
             $("#temp5").append('Temperature: ' + (((response.list[32].main.temp - 273.15) * 1.8 + 32).toFixed(1) + `\xB0` + 'F'));
             $('#humid5').append('Humidity: ' + ((response.list[32].main.humidity) + '%'));
             $('#wind5').append('Wind speed: ' + ((response.list[32].wind.speed) + 'mph'));
@@ -62,5 +56,6 @@ $(document).ready(function () {
 });
 
 function showForecast() {
-document.querySelector('#five-day-forecast').style.visibility = 'visible';
+    document.querySelector('#today-forecast').style.visibility = 'visible';
+    document.querySelector('#five-day-forecast').style.visibility = 'visible';
 }
