3a498e3a81feff4e5c7721fd0b83aad5b54494e9
lbalbrecht <lalbrecht18@wooster.edu>
2021-05-20 16:14:18 -0700

    added icons to forecast

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# lba-weather-dashboard
+# Weather Dashboard
+
+## Description
+
+[Deployed on Github Pages](https://lbalbrecht.github.io/lba-weather-dashboard/)
+## Usage
+
+## Contributors
+
+[Github Repository](https://github.com/lbalbrecht/lba-weather-dashboard)
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,18 @@ var apiKey = '91d06b081975cb9e9ba419e831395fd0';
 $(document).ready(function () {
     $('#search-btn').on('click', function (event) {
         event.preventDefault();
+        clearPage();
         var cityInput = $("#city-input").val();
-        var searches = [];
-        searches.push(cityInput)
-        localStorage.setItem('searches', searches)
+        // var searches = [];
+        // searches.push(cityInput)
+        // localStorage.setItem('searches', searches)
         ajaxCall(cityInput);
-        console.log(cityInput);
     });
     function ajaxCall(cityInput) {
         $.ajax({
             url: `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`,
             method: "GET",
         }).then(function (response) {
-            console.log(response);
             var todayHeader = $('#name-date')
             todayHeader.append(cityInput + ' ' + (moment().format('l')));
 
@@ -27,67 +26,121 @@ $(document).ready(function () {
                 $('#weather-icon').addClass('fas fa-cloud-rain')
             }
             if (response.weather[0].main === "Clouds") {
-                $('#weather-icon').addClass('fas fa-clouds')
+                $('#weather-icon').addClass('fas fa-cloud')
             }
             if (response.weather[0].main === "Clear") {
                 $('#weather-icon').addClass('fas fa-sun')
             }
             if (response.weather[0].main === "Snow") {
-                $('#weather-icon').addClass('fas fa-snowflakes')
+                $('#weather-icon').addClass('fas fa-snowflake')
             }
-
-        });
+        }); 
         $.ajax({
             url: `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=${apiKey}`,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
             $("#date1").append((moment().add(1, 'days').format('l')));
             $("#temp1").append('Temperature: ' + (((response.list[0].main.temp - 273.15) * 1.8 + 32).toFixed(1) + `\xB0` + 'F'));
             $('#humid1').append('Humidity: ' + ((response.list[0].main.humidity) + '%'));
-            $('#wind1').append('Wind speed: ' + ((response.list[0].wind.speed) + 'mph'));
+            if (response.list[0].weather[0].main === "Rain") {
+                $('#icon1').addClass('fas fa-cloud-rain')
+            }
+            if (response.list[0].weather[0].main === "Clouds") {
+                $('#icon1').addClass('fas fa-cloud')
+            }
+            if (response.list[0].weather[0].main === "Clear") {
+                $('#icon1').addClass('fas fa-sun')
+            }
+            if (response.list[0].weather[0].main === "Snow") {
+                $('#icon1').addClass('fas fa-snowflake')
+            }
 
             $("#date2").append((moment().add(2, 'days').format('l')));
             $("#temp2").append('Temperature: ' + (((response.list[8].main.temp - 273.15) * 1.8 + 32).toFixed(1) + `\xB0` + 'F'));
             $('#humid2').append('Humidity: ' + ((response.list[8].main.humidity) + '%'));
-            $('#wind2').append('Wind speed: ' + ((response.list[8].wind.speed) + 'mph'));
+            if (response.list[8].weather[0].main === "Rain") {
+                $('#icon2').addClass('fas fa-cloud-rain')
+            }
+            if (response.list[8].weather[0].main === "Clouds") {
+                $('#icon2').addClass('fas fa-cloud')
+            }
+            if (response.list[8].weather[0].main === "Clear") {
+                $('#icon2').addClass('fas fa-sun')
+            }
+            if (response.list[8].weather[0].main === "Snow") {
+                $('#icon2').addClass('fas fa-snowflake')
+            }
 
             $("#date3").append((moment().add(3, 'days').format('l')));
             $("#temp3").append('Temperature: ' + (((response.list[16].main.temp - 273.15) * 1.8 + 32).toFixed(1) + `\xB0` + 'F'));
             $('#humid3').append('Humidity: ' + ((response.list[16].main.humidity) + '%'));
-            $('#wind3').append('Wind speed: ' + ((response.list[16].wind.speed) + 'mph'));
+            if (response.list[16].weather[0].main === "Rain") {
+                $('#icon3').addClass('fas fa-cloud-rain')
+            }
+            if (response.list[16].weather[0].main === "Clouds") {
+                $('#icon3').addClass('fas fa-cloud')
+            }
+            if (response.list[16].weather[0].main === "Clear") {
+                $('#icon3').addClass('fas fa-sun')
+            }
+            if (response.list[16].weather[0].main === "Snow") {
+                $('#icon3').addClass('fas fa-snowflake')
+            }
 
             $("#date4").append((moment().add(4, 'days').format('l')));
             $("#temp4").append('Temperature: ' + (((response.list[24].main.temp - 273.15) * 1.8 + 32).toFixed(1) + `\xB0` + 'F'));
             $('#humid4').append('Humidity: ' + ((response.list[24].main.humidity) + '%'));
-            $('#wind4').append('Wind speed: ' + ((response.list[24].wind.speed) + 'mph'));
+            if (response.list[24].weather[0].main === "Rain") {
+                $('#icon4').addClass('fas fa-cloud-rain')
+            }
+            if (response.list[24].weather[0].main === "Clouds") {
+                $('#icon4').addClass('fas fa-cloud')
+            }
+            if (response.list[24].weather[0].main === "Clear") {
+                $('#icon4').addClass('fas fa-sun')
+            }
+            if (response.list[24].weather[0].main === "Snow") {
+                $('#icon4').addClass('fas fa-snowflake')
+            }
 
             $("#date5").append((moment().add(5, 'days').format('l')));
             $("#temp5").append('Temperature: ' + (((response.list[32].main.temp - 273.15) * 1.8 + 32).toFixed(1) + `\xB0` + 'F'));
             $('#humid5').append('Humidity: ' + ((response.list[32].main.humidity) + '%'));
-            $('#wind5').append('Wind speed: ' + ((response.list[32].wind.speed) + 'mph'));
-
+            if (response.list[32].weather[0].main === "Rain") {
+                $('#icon5').addClass('fas fa-cloud-rain')
+            }
+            if (response.list[32].weather[0].main === "Clouds") {
+                $('#icon5').addClass('fas fa-cloud')
+            }
+            if (response.list[32].weather[0].main === "Clear") {
+                $('#icon5').addClass('fas fa-sun')
+            }
+            if (response.list[32].weather[0].main === "Snow") {
+                $('#icon5').addClass('fas fa-snowflake')
+            }
         })
         showForecast();
     }
 });
 
 function showForecast() {
+    document.querySelector('#city-list').style.visibility = 'visible';
     document.querySelector('#today-forecast').style.visibility = 'visible';
     document.querySelector('#five-day-forecast').style.visibility = 'visible';
 };
 
 
-var listEl = document.createElement('li');
-var retrievedSearches = localStorage.getItem('searches');
+// var listEl = document.createElement('li');
+// var retrievedSearches = localStorage.getItem('searches');
 
-for (i = 0; i < retrievedSearches.length; i++) {
-    listEl.append(retrievedSearches[i])
-}
-$('#city-list').append(listEl)
-listEl.style.margin = '15px';
+// for (i = 0; i < retrievedSearches.length; i++) {
+//     listEl.append(retrievedSearches[i])
+// }
+// $('#city-list').append(listEl)
+// listEl.style.margin = '15px';
 
-$('body').bind('beforeunload', function () {
+function clearPage() {
+    $("#name-date").empty();
     $("#temp").empty();
     $("#humid").empty();
     $("#wind").empty();
@@ -116,5 +169,4 @@ $('body').bind('beforeunload', function () {
     $("#temp5").empty();
     $('#humid5').empty();
     $('#wind5').empty();
-
-})
+}
